f27e7efb67571541ecb960f51f97c40436524443
Golddy Dev <firingdev@gmail.com>
2025-02-11 23:19:16 +0800

    update smart contract a bit

--- src/configs/preprod.config.ts
+++ src/configs/preprod.config.ts
@@ -10,7 +10,7 @@ export const TREASURY_FEE = 1_000_000n;
 export const MINTER_FEE = 1_000_000n;
 // payment credentials who can mint
 export const ALLOWED_MINTERS = [
-  "0e1bdf3e12a8d4e915f40c721e30bfab50675f0b44b38e7bb7f67785",
+  "4da965a049dfd15ed1ee19fba6e2974a0b79fc416dd1796a1f97f5e1", // Kora labs admin
 ];
 export const TREASURY_ADDRESS = makeAddress(
   "addr_test1qq8phhe7z25df6g47sx8y83sh744qe6lpdzt8rnmklm80pvhz7gfc46pmx59ynx7tmcrcnw5j8l8jhglmugl6e7k3f0q30rg89"
--- src/configs/preview.config.ts
+++ src/configs/preview.config.ts
@@ -10,7 +10,7 @@ export const TREASURY_FEE = 1_000_000n;
 export const MINTER_FEE = 1_000_000n;
 // payment credentials who can mint
 export const ALLOWED_MINTERS = [
-  "8ba59b21136f2f0c84865fb017a5f67245660ff348d85c65c23a1411",
+  "4da965a049dfd15ed1ee19fba6e2974a0b79fc416dd1796a1f97f5e1", // Kora labs admin
 ];
 export const TREASURY_ADDRESS = makeAddress(
   "addr_test1qz96txepzdhj7ryyse0mq9a97eey2es07dydshr9cgapgyv06l2rv7x0p0vtg5kufwj3avaa909ex8uswsnxnad9ccqsyaga0j"
